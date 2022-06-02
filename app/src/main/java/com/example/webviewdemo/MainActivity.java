1afc0df585b7ba18067bd79df4c5e96b1da4345f
Lalit Kumar <lalit.kumar@avataar.me>
2022-06-02 11:38:11 +0530

    minor updates for camera request

--- app/src/main/java/com/example/webviewdemo/MainActivity.java
+++ app/src/main/java/com/example/webviewdemo/MainActivity.java
@@ -1,19 +1,49 @@
 package com.example.webviewdemo;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.PermissionRequest;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.widget.Button;
+import android.webkit.WebChromeClient;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_CODE = 101;
     WebView webView;
+    Button cameraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraBtn = findViewById(R.id.camBtn);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermissions();
+            }
+        });
+    }
+
+    public void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_CODE);
+        } else {
+            openWebView();
+        }
+    }
+
+    public void openWebView() {
         webView = findViewById(R.id.webView);
+        cameraBtn.setVisibility(View.GONE);
+        webView.setVisibility(View.VISIBLE);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
@@ -28,6 +58,12 @@ public class MainActivity extends AppCompatActivity {
                 String script = "document.getElementsByTagName('body')[0]"+".addEventListener("+
                         "'click', function () {  js.ShowToast('Calling native class method form js code');})";
                 webView.evaluateJavascript(script, null);
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
             }
         });
         webView.addJavascriptInterface(new JSBridge(this), "js");
